6f7d6e639315fa1de35774c7117e519c419e9cd0
Vinicius <vinicius_henrique96@hotmail.com>
2023-01-13 12:59:57 -0300

    :sparkles: feat: implementing accordion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,34 @@
 "use strict";
 
-const faqQuestionContainer = document.querySelector(".faq__question-box");
-const questionAll = document.querySelectorAll(".faq__question");
-const faqDescAll = document.querySelectorAll(".faq__desc");
+const faqQuestionContainer = document.querySelector(
+  "[data-question-container]"
+);
 
 faqQuestionContainer.addEventListener("click", function (e) {
-  const clicked = e.target.closest(".faq__btn");
-  console.log(clicked);
+  let clickedBtn;
+  let faqQuestionBox;
+  const classActive = "active";
+
+  clickedBtn = e.target.closest("[data-faq-btn]");
+
+  if (!clickedBtn) return;
+  faqQuestionBox = clickedBtn.closest("[data-question]");
+
+  clickedBtn.classList.toggle(classActive);
+  faqQuestionBox.nextElementSibling.classList.toggle(classActive);
+
+  if (!faqQuestionBox.nextElementSibling.classList.contains(classActive))
+    return clickedBtn.setAttribute("aria-expanded", "false");
+  clickedBtn.setAttribute("aria-expanded", "true");
+
+  document.querySelectorAll("[data-faq-btn].active").forEach((faqBtn) => {
+    if (faqBtn === clickedBtn) return;
+    faqBtn.classList.remove(classActive);
+    faqBtn.setAttribute("aria-expanded", "false");
+  });
+
+  document.querySelectorAll("[data-desc].active").forEach((desc) => {
+    if (desc === faqQuestionBox.nextElementSibling) return;
+    desc.classList.remove(classActive);
+  });
 });
